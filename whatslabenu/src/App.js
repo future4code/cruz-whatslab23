6e2d9b4d3fae117788f82382da9aefc8058747fc
Angelo O'Dwyer <70985334+AngeloVSO@users.noreply.github.com>
2021-03-12 16:27:25 -0300

    Ajuste css

--- whatslabenu/src/App.js
+++ whatslabenu/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MainApp = styled.header`
+background-color: #282c34;
+min-height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+font-size: calc(10px + 2vmin);
+color: white;
+position: relative;
+`
+const LogoAppStyle = styled.h1`
+position: absolute;
+top: -10px;
+left: 50%;
+transform: translateX(-50%);
+color: #fff;
+font-size: 2rem;
+`
+
+const BodyChat = styled.div`
+background-color: whitesmoke;
+height: 80vh;
+width: 60%;
+display: flex;
+flex-direction: column-reverse;
+padding: 10px;
+box-sizing: border-box;
+`
+
+const Paragraph = styled.p`
+color: #1a1a1a;
+margin: 5px 10px;
+`
+
+class App extends React.Component {
+  render () {
+    return (
+      <MainApp>
+        <LogoAppStyle>Bem vind@ ao WhatsLab!</LogoAppStyle>
+        <BodyChat>
+          <Paragraph>Victor: e ai?</Paragraph>
+          <Paragraph>Angelo: e ai?</Paragraph>
+        </BodyChat>
+      </MainApp>
+    );
+  } 
 }
-
 export default App;
